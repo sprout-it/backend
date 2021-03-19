af10540354e668aa6c02c2b39bf88cbf638e89ec
GuyAK10 <akkaraphol.khunpet@gmail.com>
2021-03-20 01:44:54 +0700

    create get restaurants route

--- restarant/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router()
-
-router.get('/restarants', (req, res) => {
-    res.status(200).send({ OK: "OK" })
-    res.end()
-})
-
-module.exports = router
