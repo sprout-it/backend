a45bd0b48c3c195570966430d07b7c8dd6cb4a58
Fluke <jesadakorn.kirtnu@gmail.com>
2021-04-30 23:12:58 +0700

    add functions send nofti and background + foreground message

--- utils/FCM/index.js
+++ utils/FCM/index.js
@@ -1,22 +1,79 @@
 // This registration token comes from the client FCM SDKs.
-var registrationToken = 'YOUR_REGISTRATION_TOKEN';
-const FCM_TOKEN = process.env.FCM_TOKEN || 'APA91bHiq2iJgxF_gHA8Ck3vmqF8cbVx57vwTYcShsxFlXoY1rAHCIsQ_WbMXaFZZkLQ0yrTvxQTnR5uBSaoLm3UFf36btwMHxURsi4wJ46zK5leZ23Es3W0lSvTMCHlEJdvCf7wz5Ow'
+const { admin } = require('../../configs/firebase')
+var registrationToken = 'AAAA8FGoncw:APA91bHiq2iJgxF_gHA8Ck3vmqF8cbVx57vwTYcShsxFlXoY1rAHCIsQ_WbMXaFZZkLQ0yrTvxQTnR5uBSaoLm3UFf36btwMHxURsi4wJ46zK5leZ23Es3W0lSvTMCHlEJdvCf7wz5Ow';
+const FCM_TOKEN = process.env.FCM_TOKEN || 'cKdLM0f2R_GSciu59bI3He:APA91bH6IjuUL0NcgJom5s1ZJ_Svf0LdV_HQgIwTFPGHEWTZf3kNYuzfyZ35FlNUeGCj94dVjgczC15ue2WA9qB-KX_BostyC1YpqXTmV07Mxe7ku7SIR3FcTZJIQG1BDAoYMnF8RE1Q'
 
-var message = {
-    data: {
-        score: '850',
-        time: '2:45'
-    },
-    token: registrationToken
-};
+const messaging = admin.messaging();
 
-// Send a message to the device corresponding to the provided
-// registration token.
-admin.messaging().send(message)
-    .then((response) => {
-        // Response is a message ID string.
-        console.log('Successfully sent message:', response);
-    })
-    .catch((error) => {
-        console.log('Error sending message:', error);
-    });
+const sendNotification = (data, token) => {
+    const { title, body, imageUrl } = data;
+    let message = {
+        notification: {
+            title, body, imageUrl
+        },
+        tokens: token
+    };
+    messaging
+        .sendMulticast(message)
+        .then((response) => {
+            console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+        });
+}
+
+const sendToUsers = (data, token) => {
+    let message = {
+        data,
+        token: [FCM_TOKEN],
+        contentAvailable: true,
+        priority: "high",
+    };
+    messaging
+        .send(message)
+        .then((response) => {
+            console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+        });
+}
+
+const sendTest = (data, token) => {
+    let message = {
+        data: {
+            score: '850',
+            time: '2:45'
+        },
+        token: [FCM_TOKEN]
+    };
+    messaging
+        .send(message)
+        .then((response) => {
+            console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+        });
+}
+
+const sendNotificationTest = async (data, token) => {
+    // const { title, body, imageUrl } = data;
+    let message = {
+        notification: {
+            title: 'test', body: 'testsetsetsetsetset', imageUrl: 'https://brandinside.asia/wp-content/uploads/2018/01/shippop2.jpg'
+        },
+        tokens: ['cKdLM0f2R_GSciu59bI3He:APA91bH6IjuUL0NcgJom5s1ZJ_Svf0LdV_HQgIwTFPGHEWTZf3kNYuzfyZ35FlNUeGCj94dVjgczC15ue2WA9qB-KX_BostyC1YpqXTmV07Mxe7ku7SIR3FcTZJIQG1BDAoYMnF8RE1Q']
+    };
+    await messaging
+        .sendMulticast(message)
+        .then((response) => {
+            console.log('Successfully sent message:', response);
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+        });
+}
+
+sendNotificationTest();
